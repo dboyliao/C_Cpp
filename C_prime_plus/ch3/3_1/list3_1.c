ca5b8e62a4725ed357fa79bc33ef74415978a75f
dboyliao <qmalliao@gmail.com>
2015-11-16 01:55:29 +0800

    Daily commit.

---
+++ C_prime_plus/ch3/3_1/list3_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float weight;
+    float value;
+
+    printf("Please enter your weight in kg: ");
+    scanf("%f", &weight);
+
+    value = 1700 * weight;
+    printf("Money: %.2f.\n", value);
+    return 0;
+}
